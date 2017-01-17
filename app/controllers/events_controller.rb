cfcac781794062192c1030620871fc816c693d35
ruoshuigao <jannegao@gmail.com>
2017-01-17 11:50:49 +0800

    1.完成页面持续加载；2.移除kaminari gem 手动进行数据分页

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,21 @@ class EventsController < ApplicationController
 
   def index
     current_user_resource_ids = current_user.resource_ids
-    team_events               = Event.where(team_id: params[:team_id], resource_id: current_user_resource_ids).order(id: :desc).page(params[:page]).per(50)
-    @team_events_hash         = team_events_hash_with(team_events)
+    @per_page       = 50
+    page_num        = params[:page].to_i
+    all_team_events = Event.where(team_id: params[:team_id], resource_id: current_user_resource_ids).order(id: :desc)
+    @events_count   = all_team_events.count
+    team_events =
+      if page_num <= 1
+        all_team_events.limit(@per_page)
+      else
+        all_team_events.offset(@per_page * (page_num - 1)).limit(@per_page)
+      end
+    @team_events_hash = team_events_hash_with(team_events)
+    @total_page       = (@events_count / @per_page.to_f).ceil
+
+    if request.xhr?
+      render partial: 'team', locals: { team_events_hash: @team_events_hash }
+    end
   end
 end
